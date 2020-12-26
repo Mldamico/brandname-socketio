e340533a1449a58a036e605bb01036bbe99b8685
mldamico <matias.lioneldamico@gmail.com>
2020-12-26 19:00:12 -0300

    useContext on addBand

--- bandnames/src/components/BandAdd.js
+++ bandnames/src/components/BandAdd.js
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import { useSocket } from '../hooks/useSocket';
+import { useState, useContext } from "react";
+import { SocketContext } from "../context/SocketContext";
+
 export const BandAdd = () => {
-  const [valor, setValor] = useState('');
-  const { socket } = useSocket('http://localhost:8080');
-  const onSubmit = (e) => {
+  const [valor, setValor] = useState("");
+  const { socket } = useContext(SocketContext);
+  const onSubmit = e => {
     e.preventDefault();
     if (valor.trim().length > 0) {
-      socket.emit('nueva-band', { nombre: valor });
-      setValor('');
+      socket.emit("nueva-band", { nombre: valor });
+      setValor("");
     }
   };
   return (
@@ -15,11 +16,11 @@ export const BandAdd = () => {
       <h3>Agregar Banda</h3>
       <form onSubmit={onSubmit}>
         <input
-          type='text'
-          className='form-control'
-          placeholder='Nuevo Nombre de banda'
+          type="text"
+          className="form-control"
+          placeholder="Nuevo Nombre de banda"
           value={valor}
-          onChange={(e) => setValor(e.target.value)}
+          onChange={e => setValor(e.target.value)}
         />
       </form>
     </>
